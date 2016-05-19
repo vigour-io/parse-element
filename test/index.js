02b156936d838200aecb2778787a01605ea83438
Jim de Beer <jim@vigour.io>
2016-05-19 20:15:29 +0200

    removed redundant isNode checks

--- test/index.js
+++ test/index.js
@@ -2,7 +2,6 @@
 require('html-element')
 const test = require('tape')
 const parseElement = require('../')
-const isNode = require('vigour-util/is/node')
 
 test('parses single element correctly', (t) => {
   const div = global.document.createElement('div')
@@ -18,18 +17,16 @@ test('parses single element correctly', (t) => {
 
   const textarea = global.document.createElement('textarea')
   textarea.appendChild(document.createTextNode('some text'))
-  t.equal(parseElement(textarea),
-    isNode
-      ? '<textarea>some text</textarea>'
-      : '<textarea>some text</textarea>',
+  t.equal(
+    parseElement(textarea),
+    '<textarea>some text</textarea>',
     'textarea'
   )
 
   const emptyTextarea = global.document.createElement('textarea')
-  t.equal(parseElement(emptyTextarea),
-    isNode
-      ? '<textarea></textarea>'
-      : '<textarea></textarea>',
+  t.equal(
+    parseElement(emptyTextarea),
+    '<textarea></textarea>',
     'empty textarea'
   )
 
@@ -37,7 +34,11 @@ test('parses single element correctly', (t) => {
   span.appendChild(document.createTextNode('<ha & ha>'))
   span.setAttribute('special', 100)
   span.id = 'hello'
-  t.equal(parseElement(span), '<span id="hello" special="100">&lt;ha &amp; ha&gt;</span>', 'span')
+  t.equal(
+    parseElement(span),
+    '<span id="hello" special="100">&lt;ha &amp; ha&gt;</span>',
+    'span'
+  )
 
   t.end()
 })
